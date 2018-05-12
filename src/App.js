46c837e0b0ba1671aa4e221cba0a4656ce7a2aa7
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-12 17:30:09 +0700

    Added redux-thunk middleware

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
 import { Header } from './components/common';
@@ -22,8 +23,10 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View style={{ flex: 1 }}>
           <Header text="Manager" />
           <LoginForm />
