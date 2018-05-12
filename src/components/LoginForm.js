995c5187ab1a1720f9cc429b3f1c88e2909f1881
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-12 20:01:13 +0700

    Added auth validation error feature

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 /* @flow */
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 import type { EmailChanged, PasswordChanged, LoginUser } from '../actions';
@@ -11,6 +12,7 @@ type Props = {
   loginUser: LoginUser,
   email: string,
   password: string,
+  error: string,
 }
 
 class LoginForm extends Component<Props> {
@@ -25,6 +27,20 @@ class LoginForm extends Component<Props> {
   onButtonPress = () => {
     const { email, password } = this.props;
     this.props.loginUser({ email, password });
+  }
+
+  renderError = () => {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: 'white' }}>
+          <Text style={styles.errorTextStyle}>
+            {this.props.error}
+          </Text>
+        </View>
+      );
+    }
+
+    return null;
   }
 
   render() {
@@ -49,6 +65,9 @@ class LoginForm extends Component<Props> {
             value={password}
           />
         </CardSection>
+
+        {this.renderError()}
+
         <CardSection>
           <Button onPress={this.onButtonPress}>Login</Button>
         </CardSection>
@@ -57,10 +76,19 @@ class LoginForm extends Component<Props> {
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+};
+
 const mapStateToProps = (state) => {
   return {
     email: state.auth.email,
     password: state.auth.password,
+    error: state.auth.error,
   };
 };
 
