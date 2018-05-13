6458dbb0216f158986826fd8bd5f2f1edda13bd2
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-13 23:15:08 +0700

    Replaced unneeded lifecycle methods & ListView with FlatView * ListView is deprecated since react native 0.43 * FlatView / SectionView are preferred
    [refer to https://facebook.github.io/react-native/docs/listview.html or Q&A section in lecture 149]

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,47 +1,30 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, View, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { employeesFetch } from '../actions';
 import type { EmployeesFetchType } from '../actions';
+import ListItem from './ListItem';
 
 type Props = {
+  employees: Array,
   employeesFetch: EmployeesFetchType,
 }
 
 class EmployeeList extends Component<Props> {
-  componentWillMount() {
+  componentDidMount() {
     this.props.employeesFetch();
-
-    this.createDataSource(this.props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    // nextProps are the next set of props that this component will be
-    // rendered with. this.props is still the old set of props.
-
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ employees }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-    });
-
-    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
-    console.log(this.props);
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <FlatList
+        data={this.props.employees}
+        renderItem={({ item }) => {
+          return <ListItem employee={item} />;
+        }}
+        keyExtractor={employee => employee.uid}
+      />
     );
   }
 }
