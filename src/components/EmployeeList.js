1d3434a845a47a44f1c231d0c1a8f868facdb812
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-13 18:14:17 +0700

    Made component receive (async) props from firebase fetch

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,33 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView, View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component will be
+    // rendered with. this.props is still the old set of props.
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>Employee List</Text>
@@ -16,4 +41,15 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = (state) => {
+  // for each key-value pair in val (our employee model / object) specified by
+  // state.employees, create a new object with an extra uid field and pack them
+  // all into an array.
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
