15b1dca088b45fc84d7587fb638050798f183fe7
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-13 18:13:24 +0700

    Added employees fetch reducer & action

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS,
+} from './types';
 
 // 1 Action creator that can update any different property existing in our form
 export const employeeUpdate = ({ prop, value }) => {
@@ -19,6 +23,23 @@ export const employeeCreate = ({ name, phone, shift }) => {
       .then(() => {
         dispatch({ type: EMPLOYEE_CREATE });
         Actions.pop();
+      });
+  };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    // Listens for updates to this ref endpoint for the lifetime of the
+    // application. Hence, it works quite nicely with redux since the action
+    // would only be dispatched when a change to the employees' list is made.
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', (snapshot) => {
+        dispatch({
+          type: EMPLOYEES_FETCH_SUCCESS,
+          payload: snapshot.val(),
+        });
       });
   };
 };
