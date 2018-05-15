95e66dd63ca686dd414a503ee550c18de39d2841
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-15 23:25:43 +0700

    Made employee list item clickable such that it leads to * the employee create form page again

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,23 +1,35 @@
 /* @flow */
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 type Props = {
   employee: Object,
 };
 
-const ListItem = (props: Props) => {
-  const { name } = props.employee;
+class ListItem extends Component<Props> {
+  onRowPress = () => {
+    Actions.employeeCreate({ employee: this.props.employee });
+  }
 
-  return (
-    <CardSection>
-      <Text style={styles.titleStyle}>
-        {name}
-      </Text>
-    </CardSection>
-  );
-};
+  render() {
+    const { name } = this.props.employee;
+
+    return (
+      <TouchableWithoutFeedback onPress={this.onRowPress}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
 
 const styles = {
   titleStyle: {
