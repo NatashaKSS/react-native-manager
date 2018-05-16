b1643cdda148783ae7091c0f0086684b1965d36d
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-16 17:51:24 +0700

    Refactored to pure function / component

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection, Input } from './common';
@@ -12,55 +12,57 @@ type Props = {
   employeeUpdate: EmployeeUpdateType,
 }
 
-class EmployeeForm extends Component<Props> {
-  render() {
-    const { name, phone, shift } = this.props;
+const EmployeeForm = (props: Props) => {
+  const { name, phone, shift } = props;
 
-    return (
-      <View>
-        <CardSection>
-          <Input
-            label="Name"
-            placeholder="Tom"
-            value={name}
-            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-          />
-        </CardSection>
+  return (
+    <View>
+      <CardSection>
+        <Input
+          label="Name"
+          placeholder="Tom"
+          value={name}
+          onChangeText={value => props.employeeUpdate({ prop: 'name', value })}
+        />
+      </CardSection>
 
-        <CardSection>
-          <Input
-            label="Phone"
-            placeholder="5555-5555"
-            value={phone}
-            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-          />
-        </CardSection>
+      <CardSection>
+        <Input
+          label="Phone"
+          placeholder="5555-5555"
+          value={phone}
+          onChangeText={value => props.employeeUpdate({ prop: 'phone', value })}
+        />
+      </CardSection>
 
-        <CardSection style={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerLabelStyle}>Select a shift</Text>
-          <Picker
-            style={{ height: 300, width: '100%' }}
-            selectedValue={shift}
-            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-          >
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Saturday" value="Saturday" />
-            <Picker.Item label="Sunday" value="Sunday" />
-          </Picker>
-        </CardSection>
-      </View>
-    );
-  }
-}
+      <CardSection style={{ flexDirection: 'column' }}>
+        <Text style={styles.pickerLabelStyle}>Select a shift</Text>
+        <Picker
+          style={styles.pickerStyle}
+          selectedValue={shift}
+          onValueChange={value => props.employeeUpdate({ prop: 'shift', value })}
+        >
+          <Picker.Item label="Monday" value="Monday" />
+          <Picker.Item label="Tuesday" value="Tuesday" />
+          <Picker.Item label="Wednesday" value="Wednesday" />
+          <Picker.Item label="Thursday" value="Thursday" />
+          <Picker.Item label="Friday" value="Friday" />
+          <Picker.Item label="Saturday" value="Saturday" />
+          <Picker.Item label="Sunday" value="Sunday" />
+        </Picker>
+      </CardSection>
+    </View>
+  );
+};
 
 const styles = {
   pickerLabelStyle: {
     fontSize: 18,
     paddingLeft: 20,
+  },
+  pickerStyle: {
+    height: 225,
+    width: '100%',
   },
 };
 
